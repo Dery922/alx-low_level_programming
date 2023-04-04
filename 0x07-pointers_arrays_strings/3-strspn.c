d191d457e2186b7b3da3da938423c7dff5297e6a
Dery922 <franklindery922@gmail.com>
2023-04-03 19:24:06 -0700

    pushing all at once

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int w = 0;
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+			{
+				w++;
+				break;
+			}
+			else if (accept[x + 1] == '\0')
+				return (w);
+		}
+		s++;
+	}
+	return (w);
+}
